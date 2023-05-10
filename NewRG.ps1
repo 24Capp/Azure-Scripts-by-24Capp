5efff2631a88ff31756c5a937d026e684b5ef4ce
24Capp <130304887+24Capp@users.noreply.github.com>
2023-05-10 11:44:06 +0200

    Update NewRG.ps1

--- NewRG.ps1
+++ NewRG.ps1
@@ -11,26 +11,3 @@ $location = "westus2"
 
 # Crea il gruppo di risorse
 New-AzResourceGroup -Name $resourceGroupName -Location $location
-
-
-#Ringraziamenti 24Capp
-Add-Type -AssemblyName System.Windows.Forms
-
-$message = "24Capp Script Official"
-
-$popup = New-Object System.Windows.Forms.Form
-$popup.Text = "24Capp Thanks, everyone"
-$popup.Width = 400
-$popup.Height = 200
-$popup.BackColor = [System.Drawing.Color]::Yellow
-$popup.ForeColor = [System.Drawing.Color]::Red
-
-$label = New-Object System.Windows.Forms.Label
-$label.Location = New-Object System.Drawing.Point(20, 50)
-$label.Size = New-Object System.Drawing.Size(350, 50)
-$label.Text = $message
-$label.Font = New-Object System.Drawing.Font("Arial", 16, [System.Drawing.FontStyle]::Bold)
-$label.TextAlign = [System.Drawing.ContentAlignment]::MiddleCenter
-$popup.Controls.Add($label)
-
-$popup.ShowDialog()
